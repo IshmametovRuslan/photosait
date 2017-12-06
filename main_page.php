83b47ca49f76b5544b248e16c35f4061455d316a
Ruslan Ishmametov <russ-i@yandex.ru>
2017-12-06 21:48:07 +0300

    Верстка главной страницы

--- main_page.php
+++ main_page.php
@@ -1,3 +1,13 @@
-<div class="main_background">
-
+<div class="page text_center">
+	<div class="page_menu">
+		<div class="nav_bar">
+			<div class="menu">
+			</div>
+		</div>
+		<div class="nav_open nav_close">
+			<div class="open"></div>
+			<div class="close"></div>
+		</div>
+	</div>
+	<div class="page_content"></div>
 </div>
