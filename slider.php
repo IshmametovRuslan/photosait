6b8140456056f5e935891b74057957d6bb08f729
Ruslan Ishmametov <russ-i@yandex.ru>
2017-12-10 17:07:01 +0300

    Верстка слайдера

--- slider.php
+++ slider.php
@@ -1,46 +1,26 @@
-<div id="testCarousel" class="carousel slide" data-ride="carousel">
-	<!-- Индикаторы карусели -->
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
 	<ol class="carousel-indicators">
-		<!-- Перейти к слайду №0 с помощью соответствующего атрибута с индексом data-slide-to="0" -->
-		<li data-target="#testCarousel" data-slide-to="0" class="active"></li>
-		<!-- Перейти к слайду №1 с помощью соответствующего индекса data-slide-to="1" -->
-		<li data-target="#testCarousel" data-slide-to="1"></li>
-		<!-- Перейти к слайду №1 с помощью соответствующего индекса data-slide-to="2" -->
-		<li data-target="#testCarousel" data-slide-to="2"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
 	</ol>
-
-	<!-- Слайды карусели -->
-	<div class="carousel-inner">
-		<!-- Слайд 1 -->
-		<div class="item active">
-			<img src="img/carousel-bootstrap-slide1.jpg" alt="...">
-			<div class="carousel-caption">
-				Дополнительная информация слайда 1
-			</div>
+	<div class="carousel-inner" role="listbox">
+		<div class="carousel-item active">
+			<img src="images/slider-images/slide_1.jpg" alt="First slide">
 		</div>
-		<!-- Слайд 2 -->
-		<div class="item">
-			<img src="img/carousel-bootstrap-slide2.jpg" alt="...">
-			<div class="carousel-caption">
-				Дополнительная информация слайда 2
-			</div>
+		<div class="carousel-item">
+			<img src="images/slider-images/slide_2.jpg" alt="Second slide">
 		</div>
-		<!-- Слайд 3 -->
-		<div class="item">
-			<img src="img/carousel-bootstrap-slide3.jpg" alt="...">
-			<div class="carousel-caption">
-				Дополнительная информация слайда 3
-			</div>
+		<div class="carousel-item">
+			<img src="images/slider-images/slide_3.jpeg" alt="Third slide">
 		</div>
 	</div>
-
-	<!-- Навигация карусели (следующий или предыдущий слайд) -->
-	<!-- Кнопка, переход на предыдущий слайд с помощью атрибута data-slide="prev" -->
-	<a class="left carousel-control" href="#testCarousel" role="button" data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left"></span>
+	<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<span class="icon-prev" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
 	</a>
-	<!-- Кнопка, переход на следующий слайд с помощью атрибута data-slide="next" -->
-	<a class="right carousel-control" href="#testCarousel" role="button" data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right"></span>
+	<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		<span class="icon-next" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
 	</a>
 </div>
