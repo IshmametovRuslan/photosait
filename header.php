3965ca830d728c37636125487d72e3e6909e3a15
Ruslan Ishmametov <russ-i@yandex.ru>
2017-12-11 21:46:04 +0300

    Доработка слайдера

--- header.php
+++ header.php
@@ -6,11 +6,9 @@
 	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/my-slider.css">
 	<link rel="stylesheet" href="css/style.css">
 	<script src="javascript/jquery-3.2.1.min.js"></script>
 	<script src="javascript/bootstrap.js"></script>
-	<script src="javascript/ism-2.2.min.js"></script>
 	<script src="javascript/script.js"></script>
 	<title>My photogallery</title>
 </head>
