ded1291cd586600af167dfb7f37b50a3d9629f32
Ruslan Ishmametov <russ-i@yandex.ru>
2017-12-11 20:59:21 +0300

    Работа над слайдером

--- slider.php
+++ slider.php
@@ -1,22 +1,22 @@
-<div id="myCarousel" class="carousel slide" data-interval="3000" data-ride="carousel">
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-	</ol>
-	<div class="carousel-inner">
-		<div class="active item">
-			<img src="images/slider-images/cat-2971067_1280.jpg" width="100%" height="100%">
-		</div>
-		<div class="item">
-			<img src="images/slider-images/photographer-2959776_1280.jpg" width="100%" height="100%">
-		</div>
-		<div class="item">
-			<img src="images/slider-images/classic-car-3000200_1280.jpg" width="100%" height="100%">
-		</div>
+<div class='container'>
+
+	<div class="ism-slider" data-transition_type="fade" data-play_type="loop" id="my-slider">
+		<ol>
+			<li>
+				<img src="images/slider-images/flower-729514_1280.jpg">
+			</li>
+			<li>
+				<img src="images/slider-images/beautiful-701678_1280.jpg">
+			</li>
+			<li>
+				<img src="images/slider-images/summer-192179_1280.jpg">
+			</li>
+			<li>
+				<img src="images/slider-images/city-690332_1280.jpg">
+			</li>
+			<li>
+				<img src="images/slider-images/bora-bora-685303_1280.jpg">
+			</li>
+		</ol>
 	</div>
-	<a class="carousel-control left" href="#myCarousel" data-slide="prev">
-	</a>
-	<a class="carousel-control right" href="#myCarousel" data-slide="next">
-	</a>
 </div>
