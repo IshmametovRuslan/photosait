18863d11a4a8f8010e34a7afc9a975dfbafc34f8
Ruslan Ishmametov <russ-i@yandex.ru>
2017-12-17 18:33:34 +0300

    1. Индикаторы слайдера

--- slider.php
+++ slider.php
@@ -1,16 +1,23 @@
 <div class="slider__container">
 	<div class="slider__box">
-		<div class="slide"
-		     style="background-image: url('images/slider-images/285449_pejzazh_vid_krasivo_kartinka_oboi_priroda_1920x1080_(www.GdeFon.ru).jpg')"></div>
-		<div class="slide"
+		<div class="slide" id="slide_1"
+		     style="background-image: url('images/slider-images/285449_pejzazh_vid_krasivo_kartinka_oboi_priroda_1920x1080_(www.GdeFon.ru).jpg')" ></div>
+		<div class="slide" id="slide_2"
 		     style="background-image: url('images/slider-images/4178065-mountain-reflections-HD.jpg')"></div>
-		<div class="slide" style="background-image: url('images/slider-images/fonstola.ru-79597.jpg')"></div>
-		<div class="slide" style="background-image: url('images/slider-images/IMG_490761.jpg"></div>
-		<div class="slide"
+		<div class="slide" id="slide_3" style="background-image: url('images/slider-images/fonstola.ru-79597.jpg')"></div>
+		<div class="slide" id="slide_4" style="background-image: url('images/slider-images/IMG_490761.jpg"></div>
+		<div class="slide" id="slide_5"
 		     style="background-image: url('images/slider-images/Lake-pier-mountains-sky-stars-night_1600x900.jpg"></div>
 	</div>
 	<div class="nav__button">
 		<div class="next__button"><div class="vertical_align"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span></div></div>
 		<div class="prev__button"><div class="vertical_align"></div></div>
 	</div>
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"><a href="#slide_1"></a></li>
+		<li data-target="#carousel-example-generic" data-slide-to="1"><a href="#slide_2"></a></li>
+		<li data-target="#carousel-example-generic" data-slide-to="2"><a href="#slide_3"></a></li>
+		<li data-target="#carousel-example-generic" data-slide-to="3"><a href="#slide_4"></a></li>
+		<li data-target="#carousel-example-generic" data-slide-to="4"><a href="#slide_5"></a></li>
+	</ol>
 </div>
