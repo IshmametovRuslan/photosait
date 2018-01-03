17889e971ac39ff49233e9c98ae82e2bfebdc300
Ruslan Ishmametov <russ-i@yandex.ru>
2018-01-03 14:24:30 +0300

    Функция для навигационных кнопок

--- main_page.php
+++ main_page.php
@@ -1,26 +1,11 @@
-<div class="page text_center">
-<!--	<div class="page_menu">
-		<div class="nav_bar">
-			<div class="menu">
-			</div>
-		</div>
-		<div class="nav_open nav_close">
-			<div class="open"></div>
-			<div class="close"></div>
-		</div>
+<div class="mainPage">
+	<div class="mainText">
+
 	</div>
-	<div class="main_page_text">
-		<div class="vertical_align">
-			<div class="name_site">
-				<h1>Ишмаметов Руслан</h1>
-				<h3>Галерея фотографий</h3>
-			</div>
-			<div class="button_gallery">
-				<button type="button" class="btn btn-outline-secondary btn-lg">Галерея</button>
-			</div>
-		</div>
-	</div>-->
-	<div class="page_content">
+	<div class="galleryBtn">
+
+	</div>
+	<div class="slider">
 		<?php include 'slider.php'; ?>
 	</div>
 </div>
--- slider.php
+++ slider.php
@@ -1,20 +1,20 @@
-<div id="block-for-slider">
+<div id="blockForSlider">
 	<div id="viewport">
 		<ul id="slidewrapper">
-			<li class="slide"><div style="background-image: url('images/slider-images/architecture-3026354.jpg')" alt="1" class="slide-img"></li>
-			<li class="slide"><div style="background-image: url('images/slider-images/DSC_9271.jpg')" alt="2" class="slide-img"></li>
-			<li class="slide"><div style="background-image: url('images/slider-images/macro-3051526.jpg')" alt="3" class="slide-img"></li>
-			<li class="slide"><div style="background-image: url('images/slider-images/people-3025988.jpg')" alt="4" class="slide-img"></li>
+			<li class="slide"><div style="background-image: url('images/slider-images/architecture-3026354.jpg')" alt="1" class="slideImg"></li>
+			<li class="slide"><div style="background-image: url('images/slider-images/DSC_9271.jpg')" alt="2" class="slideImg"></li>
+			<li class="slide"><div style="background-image: url('images/slider-images/macro-3051526.jpg')" alt="3" class="slideImg"></li>
+			<li class="slide"><div style="background-image: url('images/slider-images/people-3025988.jpg')" alt="4" class="slideImg"></li>
 		</ul>
 		<div id="prev-next-btns">
-			<div id="prev-btn"></div>
-			<div id="next-btn"></div>
+			<div id="prevBtn"></div>
+			<div id="nextBtn"></div>
 		</div>
-		<ul id="nav-btns">
-			<li class="slide-nav-btn"></li>
-			<li class="slide-nav-btn"></li>
-			<li class="slide-nav-btn"></li>
-			<li class="slide-nav-btn"></li>
+		<ul id="navBtns">
+			<li class="slideNavBtn"></li>
+			<li class="slideNavBtn"></li>
+			<li class="slideNavBtn"></li>
+			<li class="slideNavBtn"></li>
 		</ul>
 	</div>
 </div>
