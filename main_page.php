02835f07f089a993950312546e131b3d877b9232
Ruslan Ishmametov <russ-i@yandex.ru>
2018-01-03 15:17:37 +0300

    Текст и кнопка на главной странице

--- main_page.php
+++ main_page.php
@@ -1,26 +1,16 @@
-<div class="page text_center">
-<!--	<div class="page_menu">
-		<div class="nav_bar">
-			<div class="menu">
+<div class="main-page-content">
+	<div class="main-page-text">
+		<div>
+			<h2>Руслан Ишмаметов</h2>
+			<h3>Моя фотогалерея</h3>
+			<div class="gallery-btn">
+				<button class="gallery-btn">В галерею</button>
 			</div>
 		</div>
-		<div class="nav_open nav_close">
-			<div class="open"></div>
-			<div class="close"></div>
-		</div>
+
 	</div>
-	<div class="main_page_text">
-		<div class="vertical_align">
-			<div class="name_site">
-				<h1>Ишмаметов Руслан</h1>
-				<h3>Галерея фотографий</h3>
-			</div>
-			<div class="button_gallery">
-				<button type="button" class="btn btn-outline-secondary btn-lg">Галерея</button>
-			</div>
-		</div>
-	</div>-->
-	<div class="page_content">
+
+	<div class=main-page-slider">
 		<?php include 'slider.php'; ?>
 	</div>
 </div>
