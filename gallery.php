a36028decce4ec60c38e194bb49827b872dede3f
Ruslan Ishmametov <russ-i@yandex.ru>
2018-01-06 22:43:15 +0300

    Верстка галереи картинок

--- gallery.php
+++ gallery.php
@@ -1,1 +1,16 @@
-<?php
+<div class="gallery-block">
+	<div class="container gallery-grid">
+		<div class="row">
+			<div class="col-md-3 col-sm-6 col-xs-12 img-block"><img src="https://pimg.mycdn.me/getImage?url=http%3A%2F%2Fi.mycdn.me%2Fimage%3Fid%3D848709219439%26ts%3D0003000000005a02bc015e%26plc%3DUNKNOWN%26tkn%3D*POXzkU8AFucO3J_y4SH8mr6v8PA%26fn%3Dtopic_link_1080&type=TOPIC_LINK_548x274&signatureToken=qo_q56QjaXookym83EHjDA" alt=""></div>
+			<div class="col-md-3 col-sm-6 col-xs-12 img-block"><img src="https://cdn.suwalls.com/wallpapers/nature/antarctica-22790-400x250.jpg" alt=""></div>
+			<div class="col-md-3 col-sm-6 col-xs-12 img-block"><img src="https://memoryreflections.files.wordpress.com/2014/05/3d_panoramic_landscape-3200x1200.jpg?w=705&h=344" alt=""></div>
+			<div class="col-md-3 col-sm-6 col-xs-12 img-block"><img src="http://www.explor8ion.com/sites/default/files/images/scrambles/michener/michener-064.jpg" alt=""></div>
+
+			<div class="col-md-3 col-sm-6 col-xs-12 img-block"><img src="https://pimg.mycdn.me/getImage?url=http%3A%2F%2Fi.mycdn.me%2Fimage%3Fid%3D848709219439%26ts%3D0003000000005a02bc015e%26plc%3DUNKNOWN%26tkn%3D*POXzkU8AFucO3J_y4SH8mr6v8PA%26fn%3Dtopic_link_1080&type=TOPIC_LINK_548x274&signatureToken=qo_q56QjaXookym83EHjDA" alt=""></div>
+			<div class="col-md-3 col-sm-6 col-xs-12 img-block"><img src="https://cdn.suwalls.com/wallpapers/nature/antarctica-22790-400x250.jpg" alt=""></div>
+			<div class="col-md-3 col-sm-6 col-xs-12 img-block"><img src="https://memoryreflections.files.wordpress.com/2014/05/3d_panoramic_landscape-3200x1200.jpg?w=705&h=344" alt=""></div>
+			<div class="col-md-3 col-sm-6 col-xs-12 img-block"><img src="http://www.explor8ion.com/sites/default/files/images/scrambles/michener/michener-064.jpg" alt=""></div>
+
+		</div>
+	</div>
+</div>
