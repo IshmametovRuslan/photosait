3ab56b938f71fd1bf9014a19d419778adbd23908
Ruslan Ishmametov <russ-i@yandex.ru>
2018-01-06 20:04:33 +0300

    xren

--- about_me.php
+++ about_me.php
@@ -1,12 +1,20 @@
-<div class="about">
-	<div class="container about-text">
-		<h3>Обо мне</h3>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aperiam consequuntur dicta ea, labore
-			maiores minima natus non obcaecati odit quisquam reprehenderit repudiandae rerum sapiente temporibus ut vero
-			voluptatibus.
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid beatae cum dignissimos dolore doloremque
-			explicabo impedit ipsam neque, nisi nostrum pariatur quae, quaerat quam quasi repellat reprehenderit saepe
-			vitae? Animi.
-		</p>
+<div class="about-me-block">
+	<div class="container about-me-content">
+		<h2>Обо мне</h2>
+		<div class="about-text-img">
+			<div class="about-me-photo">
+			</div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci aperiam consequuntur dicta ea,
+				labore
+				maiores minima natus non obcaecati odit quisquam reprehenderit repudiandae rerum sapiente temporibus ut
+				vero
+				voluptatibus.
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid beatae cum dignissimos dolore
+				doloremque
+				explicabo impedit ipsam neque, nisi nostrum pariatur quae, quaerat quam quasi repellat reprehenderit
+				saepe
+				vitae? Animi.
+			</p>
+		</div>
 	</div>
 </div>
